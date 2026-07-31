bd0d31ab0101c3aaedcf864ebd1649a52f8bc88f
boatette <boatette@gmail.com>
2026-08-01 02:30:09 +1000

    refactor: add neovim nightly overlay and package

--- flake.nix
+++ flake.nix
@@ -7,13 +7,21 @@
         url = "github:nix-community/home-manager/release-26.05";
         inputs.nixpkgs.follows = "nixpkgs";
       };
+
       zen-browser = {
         url = "github:0xc000022070/zen-browser-flake";
         inputs.nixpkgs.follows = "nixpkgs";
       };
+      neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = { self, nixpkgs, home-manager, zen-browser, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, zen-browser, ... }@inputs:
+    let
+      overlays = [
+        inputs.neovim-nightly-overlay.overlays.default
+      ];
+in
+  {
     nixosConfigurations.redux = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
@@ -26,8 +34,12 @@
               useUserPackages = true;
               extraSpecialArgs = { inherit inputs; };
               users.boatette = ./home.nix;
+              backupFileExtension = "bak";
             };
           }
+{
+          nixpkgs.overlays = overlays;
+}
         ];
       };
     };
--- home.nix
+++ home.nix
@@ -1,6 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
+  home.packages = with pkgs; [
+    inputs.neovim-nightly-overlay.packages.${pkgs.system}.default 
+  ];
+
   home.stateVersion = "26.05";
 }
 
