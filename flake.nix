1a8ba51c336b1015032c286fbac7469abc615a25
boatette <boatette@gmail.com>
2026-08-01 02:17:44 +1000

    feat: add home-manager flake module

--- flake.nix
+++ flake.nix
@@ -2,17 +2,32 @@
   description = "NixOS Flake";
 
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-      zen-browser.url = "github:0xc000022070/zen-browser-flake";
+      nixpkgs.url = "github:NixOS/nixpkgs/26.05";
+      home-manager = {
+        url = "github:nix-community/home-manager/release-26.05";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+      zen-browser = {
+        url = "github:0xc000022070/zen-browser-flake";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
   };
 
-  outputs = { self, nixpkgs, zen-browser, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, zen-browser, ... }@inputs: {
     nixosConfigurations.redux = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
+          home-manager.nixosModules.default
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = { inherit inputs; };
+              users.boatette = ./home.nix;
+            };
+          }
         ];
       };
     };
