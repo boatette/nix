41d31c600b164e6d741bbd307752e90187fd9dc1
boatette <boatette@gmail.com>
2026-08-01 19:18:17 +1000

    feat: use nixCats to configure neovim

--- home.nix
+++ home.nix
@@ -26,6 +26,8 @@ let
 in
 
 {
+    imports = [ inputs.nixCats.homeModule ];
+
     home.username = "boatette";
     home.homeDirectory = "/home/boatette";
 
@@ -40,7 +42,6 @@ in
 
     home.packages = with pkgs; [
         foot
-        inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.default
         inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".default
         inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
         inputs.claude-code.packages.${pkgs.stdenv.hostPlatform.system}.default
@@ -122,6 +123,132 @@ in
             "fish/conf.d".source = create_symlink "${dotfiles}/fish/conf.d";
             "fish/functions".source = create_symlink "${dotfiles}/fish/functions";
         };
+
+    nixCats = {
+        enable = true;
+
+        addOverlays = [ (inputs.nixCats.utils.sanitizedPluginOverlay inputs) ];
+
+        packageNames = [ "nvim" ];
+
+        luaPath = ./config/nvim;
+
+        categoryDefinitions.replace = (
+            { pkgs, ... }:
+            let
+                pythonEnv = pkgs.python3.withPackages (ps: [ ps.debugpy ]);
+            in
+            {
+                lspsAndRuntimeDeps = {
+                    general = with pkgs; [
+                        bash-language-server
+                        clang-tools
+                        fish-lsp
+                        glsl_analyzer
+                        jdt-language-server
+                        lua-language-server
+                        nixd
+                        ols
+                        pyright
+                        ruff
+                        rust-analyzer
+                        typescript-language-server
+                        vscode-langservers-extracted
+                        zls
+                        kdePackages.qtdeclarative
+
+                        cpplint
+                        eslint_d
+                        ktlint
+                        markdownlint-cli2
+
+                        google-java-format
+                        prettierd
+                        shfmt
+                        stylua
+
+                        lldb
+                        vscode-js-debug
+                        pythonEnv
+                    ];
+                };
+
+                startupPlugins = {
+                    general = with pkgs.vimPlugins; [
+                        mini-icons
+                        snacks-nvim
+
+                        catppuccin-nvim
+                        pkgs.neovimPlugins.everforest-nvim
+                        kanagawa-nvim
+                        pkgs.neovimPlugins.github-monochrome-nvim
+                        nord-nvim
+                        rose-pine
+                        tokyonight-nvim
+                        mini-base16
+
+                        mini-ai
+                        mini-hipatterns
+                        mini-splitjoin
+                        mini-surround
+                        mini-snippets
+                        dial-nvim
+                        nvim-spider
+                        pkgs.neovimPlugins.select-undo-nvim
+                        undotree
+                        oil-nvim
+                        harpoon2
+                        bufferline-nvim
+                        todo-comments-nvim
+                        markview-nvim
+
+                        blink-lib
+                        blink-cmp
+
+                        nvim-lspconfig
+                        nvim-lint
+                        conform-nvim
+                        trouble-nvim
+
+                        nvim-treesitter.withAllGrammars
+                        nvim-treesitter-context
+
+                        nvim-dap
+                        nvim-dap-view
+
+                        neotest
+                        neotest-rust
+                        neotest-zig
+                        neotest-dart
+
+                        rustaceanvim
+                        flutter-tools-nvim
+                        nvim-java
+                        nui-nvim
+
+                        plenary-nvim
+                        nvim-nio
+                    ];
+                };
+            }
+        );
+
+        packageDefinitions.replace = {
+            nvim =
+                { pkgs, ... }:
+                {
+                    settings = {
+                        suffix-path = true;
+                        suffix-LD = true;
+                        wrapRc = false;
+                        neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.neovim;
+                    };
+                    categories = {
+                        general = true;
+                    };
+                };
+        };
+    };
 
     home.stateVersion = "26.05";
 }
