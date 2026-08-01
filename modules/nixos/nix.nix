ad8750816472a931bcd5e862311e3027e49a08ab
boatette <boatette@gmail.com>
2026-08-02 01:17:12 +1000

    feat: enable cachix

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,10 +1,21 @@
 { inputs, ... }:
 
 {
-    nix.settings.experimental-features = [
-        "nix-command"
-        "flakes"
-    ];
+    nix.settings = {
+        experimental-features = [
+            "nix-command"
+            "flakes"
+
+        ];
+        extra-substituters = [
+            "https://nix-community.cachix.org"
+            "https://noctalia.cachix.org"
+        ];
+        extra-trusted-public-keys = [
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+        ];
+    };
 
     nixpkgs = {
         config.allowUnfree = true;
