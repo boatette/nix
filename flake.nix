515b88a9e3d243b8d1b8228b987ff7870f87dc34
boatette <boatette@gmail.com>
2026-08-01 18:40:02 +1000

    pkg: add noctalia-greeter

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
             url = "github:noctalia-dev/noctalia";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+        noctalia-greeter = {
+            url = "github:noctalia-dev/noctalia-greeter";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
         zen-browser = {
             url = "github:0xc000022070/zen-browser-flake";
             inputs.nixpkgs.follows = "nixpkgs";
