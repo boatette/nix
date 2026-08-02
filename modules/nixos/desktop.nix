475e43f4b00ac8df8b867adce4814218c50bce49
boatette <boatette@gmail.com>
2026-08-02 12:50:05 +1000

    fix: add gstreamer variables

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -1,7 +1,26 @@
-{ pkgs, inputs, ... }:
+{
+    pkgs,
+    lib,
+    inputs,
+    ...
+}:
 
 {
     imports = [ inputs.noctalia-greeter.nixosModules.default ];
+
+    environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
+        lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0"
+            (
+                with pkgs.gst_all_1;
+                [
+                    gstreamer
+                    gst-plugins-base
+                    gst-plugins-good
+                    gst-plugins-bad
+                    gst-plugins-ugly
+                    gst-libav
+                ]
+            );
 
     programs = {
         appimage = {
