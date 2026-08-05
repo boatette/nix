bfb25e16dee19c592028f1987169afbc900fbf9f
boatette <boatette@gmail.com>
2026-08-05 13:33:46 +1000

    feat: switch back to 26.05

--- flake.nix
+++ flake.nix
@@ -2,11 +2,10 @@
     description = "NixOS Flake";
 
     inputs = {
-        # nixpkgs.url = "github:NixOS/nixpkgs/26.05";
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        nixpkgs-unstable.follows = "nixpkgs";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+        nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
         home-manager = {
-            url = "github:nix-community/home-manager/master";
+            url = "github:nix-community/home-manager/release-26.05";
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
