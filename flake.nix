c373707437d7e2e5e2e82b03bed2140cb269b059
boatette <boatette@gmail.com>
2026-08-07 21:56:24 +1000

    fix: follow nixpkgs for wrapper-modules

--- flake.nix
+++ flake.nix
@@ -7,7 +7,10 @@
 
         flake-parts.url = "github:hercules-ci/flake-parts";
         import-tree.url = "github:vic/import-tree";
-        wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+        wrapper-modules = {
+            url = "github:BirdeeHub/nix-wrapper-modules";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 
         home-manager = {
             url = "github:nix-community/home-manager/release-26.05";
