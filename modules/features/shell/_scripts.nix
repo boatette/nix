b8f1c61f363f70af23aacf2906d6cb0d065adcc3
boatette <boatette@gmail.com>
2026-08-07 21:49:56 +1000

    fix: disable spellcheck for ps grep

--- modules/features/shell/_scripts.nix
+++ modules/features/shell/_scripts.nix
@@ -111,6 +111,7 @@ in
         name = "psg";
         meta.description = "grep the process list without matching the grep itself";
         text = ''
+            # shellcheck disable=SC2009
             ps aux | grep -v grep | grep -i -- "$@"
         '';
     })
