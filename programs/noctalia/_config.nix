ff7e62c781ff7863fd30b6d922dec6103229d8f3
boatette <boatette@gmail.com>
2026-08-09 15:10:32 +1000

    feat: configure zellij with home-manager

--- programs/noctalia/_config.nix
+++ programs/noctalia/_config.nix
@@ -298,6 +298,7 @@
             "steam"
             "yazi"
             "bat"
+            "zellij"
         ];
 
         user = {
--- programs/zellij.nix
+++ programs/zellij.nix
@@ -1,19 +1,40 @@
 {
-    flake.homeModules.dev =
-        { pkgs, ... }:
-        {
-            home.packages = [
-                pkgs.zellij
+    flake.homeModules.dev = {
+        programs.zellij = {
+            enable = true;
 
-                (pkgs.writeShellApplication {
-                    name = "zellij-clear-all-sessions";
-                    meta.description = "kill and delete every zellij session";
-                    runtimeInputs = [ pkgs.zellij ];
-                    text = ''
-                        zellij kill-all-sessions
-                        zellij delete-all-sessions
-                    '';
-                })
-            ];
+            settings = {
+                pane_frames = false;
+                default_layout = "compact";
+                show_release_notes = false;
+                show_startup_tips = false;
+
+                ui.pane_frames = {
+                    rounded_corners = true;
+                    hide_session_name = true;
+                };
+
+                copy_on_select = true;
+                scroll_buffer_size = 50000;
+
+                simplified_ui = true;
+
+                theme = "noctalia";
+
+                themes.terminal = {
+                    fg = 7;
+                    bg = 0;
+                    black = 0;
+                    red = 1;
+                    green = 2;
+                    yellow = 3;
+                    blue = 4;
+                    magenta = 5;
+                    cyan = 6;
+                    white = 7;
+                    orange = 3;
+                };
+            };
         };
+    };
 }
