78d72112d52a3695d97195d71765a9de2e1793ce
boatette <boatette@gmail.com>
2026-08-09 15:11:53 +1000

    feat: configure foot with home-manager

--- programs/foot.nix
+++ programs/foot.nix
@@ -1,7 +1,26 @@
 {
     flake.homeModules.desktop =
-        { pkgs, ... }:
         {
-            home.packages = [ pkgs.foot ];
+            config,
+            ...
+        }:
+        {
+            programs.foot = {
+                enable = true;
+
+                settings = {
+                    main = {
+                        include = "${config.xdg.configHome}/foot/themes/noctalia";
+
+                        font = "JetBrainsMono Nerd Font:size=12";
+                        pad = "14x14";
+                    };
+
+                    colors-dark = {
+                        alpha = 0.8;
+                        blur = "yes";
+                    };
+                };
+            };
         };
 }
