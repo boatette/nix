4d39cc758e05f44211fa81a466c6a8b309ad3f6f
boatette <boatette@gmail.com>
2026-08-12 11:54:07 +1000

    refactor: update hardware config

--- hosts/aspire/hardware-configuration.nix
+++ hosts/aspire/hardware-configuration.nix
@@ -6,6 +6,7 @@
             modulesPath,
             ...
         }:
+
         {
             imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
@@ -18,30 +19,31 @@
                     "usb_storage"
                     "sd_mod"
                 ];
+                initrd.kernelModules = [ ];
                 kernelModules = [ "kvm-intel" ];
                 extraModulePackages = [ ];
             };
 
             fileSystems = {
                 "/" = {
-                    device = "/dev/disk/by-uuid/d8191e1a-7791-488b-8a7a-77dddee1b7e1";
+                    device = "/dev/disk/by-uuid/e78a042b-9725-4f1c-9f8a-3acfd03d3f2b";
                     fsType = "btrfs";
                 };
 
                 "/home" = {
-                    device = "/dev/disk/by-uuid/d8191e1a-7791-488b-8a7a-77dddee1b7e1";
+                    device = "/dev/disk/by-uuid/e78a042b-9725-4f1c-9f8a-3acfd03d3f2b";
                     fsType = "btrfs";
                     options = [ "subvol=home" ];
                 };
 
                 "/nix" = {
-                    device = "/dev/disk/by-uuid/d8191e1a-7791-488b-8a7a-77dddee1b7e1";
+                    device = "/dev/disk/by-uuid/e78a042b-9725-4f1c-9f8a-3acfd03d3f2b";
                     fsType = "btrfs";
                     options = [ "subvol=nix" ];
                 };
 
                 "/boot" = {
-                    device = "/dev/disk/by-uuid/93F9-01EF";
+                    device = "/dev/disk/by-uuid/7F78-6C51";
                     fsType = "vfat";
                     options = [
                         "fmask=0077"
