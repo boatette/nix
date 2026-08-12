6b62a5aab2708635ae2d3c42fb423790e61b6a7a
boatette <boatette@gmail.com>
2026-08-12 17:30:29 +1000

    refactor: update hardware config

--- hosts/aspire/hardware-configuration.nix
+++ hosts/aspire/hardware-configuration.nix
@@ -11,39 +11,42 @@
             imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
             boot = {
-                initrd.availableKernelModules = [
-                    "xhci_pci"
-                    "thunderbolt"
-                    "nvme"
-                    "uas"
-                    "usb_storage"
-                    "sd_mod"
-                ];
-                initrd.kernelModules = [ ];
+                initrd = {
+                    availableKernelModules = [
+                        "xhci_pci"
+                        "thunderbolt"
+                        "nvme"
+                        "usb_storage"
+                        "sd_mod"
+                    ];
+                    kernelModules = [ ];
+                    luks.devices."luks-2462d0d4-9733-413c-a67c-cdca469be2c7".device =
+                        "/dev/disk/by-uuid/2462d0d4-9733-413c-a67c-cdca469be2c7";
+                };
                 kernelModules = [ "kvm-intel" ];
                 extraModulePackages = [ ];
             };
 
             fileSystems = {
                 "/" = {
-                    device = "/dev/disk/by-uuid/e78a042b-9725-4f1c-9f8a-3acfd03d3f2b";
+                    device = "/dev/mapper/luks-2462d0d4-9733-413c-a67c-cdca469be2c7";
                     fsType = "btrfs";
                 };
 
                 "/home" = {
-                    device = "/dev/disk/by-uuid/e78a042b-9725-4f1c-9f8a-3acfd03d3f2b";
+                    device = "/dev/mapper/luks-2462d0d4-9733-413c-a67c-cdca469be2c7";
                     fsType = "btrfs";
                     options = [ "subvol=home" ];
                 };
 
                 "/nix" = {
-                    device = "/dev/disk/by-uuid/e78a042b-9725-4f1c-9f8a-3acfd03d3f2b";
+                    device = "/dev/mapper/luks-2462d0d4-9733-413c-a67c-cdca469be2c7";
                     fsType = "btrfs";
                     options = [ "subvol=nix" ];
                 };
 
                 "/boot" = {
-                    device = "/dev/disk/by-uuid/7F78-6C51";
+                    device = "/dev/disk/by-uuid/4865-78C7";
                     fsType = "vfat";
                     options = [
                         "fmask=0077"
@@ -52,7 +55,9 @@
                 };
             };
 
-            swapDevices = [ ];
+            swapDevices = [
+                { device = "/dev/mapper/luks-a3e7b969-d640-4034-91a8-c6284b742039"; }
+            ];
 
             nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
             hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
