3a9095e0074beeb6c628305688a4fe5ac4eebaac
boatette <boatette@gmail.com>
2026-08-12 17:47:55 +1000

    feat: allowDiscards for luks device

--- hosts/aspire/configuration.nix
+++ hosts/aspire/configuration.nix
@@ -65,8 +65,7 @@
                 scale = 1;
             };
 
-            # uncomment once the disk is actually encrypted
-            # boot.initrd.luks.devices."cryptroot".allowDiscards = true;
+            boot.initrd.luks.devices."luks-2462d0d4-9733-413c-a67c-cdca469be2c7".allowDiscards = true;
 
             fileSystems."/mnt/storage" = {
                 device = "/dev/disk/by-uuid/e59f23dd-5e74-46b9-92bf-386ec2fa9c27"; # not partuuid
