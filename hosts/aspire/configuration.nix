73f7e131f3c9861a77ea474f2aa6c2efe5e7fd2b
boatette <boatette@gmail.com>
2026-08-14 20:57:11 +1000

    refactor: remove luks discards

--- hosts/aspire/configuration.nix
+++ hosts/aspire/configuration.nix
@@ -69,8 +69,6 @@
         scale = 1;
       };
 
-      boot.initrd.luks.devices."luks-2462d0d4-9733-413c-a67c-cdca469be2c7".allowDiscards = true;
-
       fileSystems."/mnt/storage" = {
         device = "/dev/disk/by-uuid/e59f23dd-5e74-46b9-92bf-386ec2fa9c27"; # not partuuid
         fsType = "ext4";
