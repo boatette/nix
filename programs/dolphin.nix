d2e816b1c28e828153f03fca14b7eb5c09830b2f
boatette <boatette@gmail.com>
2026-08-16 22:09:32 +1000

    feat: add declaritive dolphin configs

--- programs/dolphin.nix
+++ programs/dolphin.nix
@@ -1,23 +1,67 @@
 {
   flake.modules.homeManager.desktop =
-    { pkgs, ... }:
     {
-      home.packages = with pkgs; [
-        (symlinkJoin {
-          name = "dolphin-kde-theme";
-          paths = [ kdePackages.dolphin ];
-          nativeBuildInputs = [ makeWrapper ];
-          postBuild = ''
-            wrapProgram $out/bin/dolphin --set QT_QPA_PLATFORMTHEME kde
-          '';
-        })
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
+    let
+      settings = {
+        dolphinrc = {
+          General = {
+            ShowFullPath = true;
+            RememberOpenedTabs = false;
+            DoubleClickViewAction = "none";
+            TabStyle = "FixedSize";
+          };
 
-        kdePackages.plasma-integration
-        kdePackages.breeze
+          MainWindow.MenuBar = "Disabled";
+        };
 
-        kdePackages.kio-fuse
-        kdePackages.kconfig
-      ];
+        kdeglobals.General = {
+          TerminalApplication = "foot";
+          TerminalService = "foot.desktop";
+        };
+      };
+
+      kwriteconfig = lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6";
+
+      writeKey = file: group: key: value: ''
+        run ${kwriteconfig} --file ${lib.escapeShellArg "${config.xdg.configHome}/${file}"} \
+            --group ${lib.escapeShellArg group} --key ${lib.escapeShellArg key} \
+            ${lib.escapeShellArg (if lib.isBool value then lib.boolToString value else toString value)}
+      '';
+
+      writeGroups =
+        file: groups:
+        lib.concatLists (
+          lib.mapAttrsToList (group: keys: lib.mapAttrsToList (writeKey file group) keys) groups
+        );
+    in
+    {
+      home = {
+        packages = with pkgs; [
+          (symlinkJoin {
+            name = "dolphin-kde-theme";
+            paths = [ kdePackages.dolphin ];
+            nativeBuildInputs = [ makeWrapper ];
+            postBuild = ''
+              wrapProgram $out/bin/dolphin --set QT_QPA_PLATFORMTHEME kde
+            '';
+          })
+
+          kdePackages.plasma-integration
+          kdePackages.breeze
+
+          kdePackages.kio-fuse
+          kdePackages.kconfig
+        ];
+
+        activation.dolphinSettings = lib.hm.dag.entryAfter [ "writeBoundary" ] (
+          lib.concatStrings (lib.concatLists (lib.mapAttrsToList writeGroups settings))
+        );
+      };
     };
 
   flake.modules.nixos.desktop =
