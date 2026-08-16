5244a8fa6b94dbc552102c535cfe3471432acc5c
boatette <boatette@gmail.com>
2026-08-16 15:06:05 +1000

    fix: always open help in vsplit

--- programs/neovim/config/after/ftplugin/help.lua
+++ programs/neovim/config/after/ftplugin/help.lua
@@ -1,5 +1,18 @@
 require("ft").utility_window()
 
-if vim.bo.buftype == "help" then
-    vim.cmd("wincmd L")
+local function vertical()
+    if vim.bo.buftype == "help" and vim.api.nvim_win_get_config(0).relative == "" then
+        vim.cmd("wincmd L")
+    end
 end
+
+local group = vim.api.nvim_create_augroup("HelpVertical", { clear = false })
+vim.api.nvim_clear_autocmds({ group = group, buffer = 0 })
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = group,
+    buffer = 0,
+    callback = vertical,
+    desc = "Open :help in a vertical split",
+})
+
+vertical()
