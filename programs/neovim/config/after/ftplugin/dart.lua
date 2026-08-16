9848fd66841e38afc4c25e331c63534a01625bd6
boatette <boatette@gmail.com>
2026-08-16 15:06:13 +1000

    fix: use 2 space indent for dart and nix

--- programs/neovim/config/after/ftplugin/dart.lua
+++ programs/neovim/config/after/ftplugin/dart.lua
@@ -1,6 +1,6 @@
-vim.opt_local.shiftwidth = 4
-vim.opt_local.tabstop = 4
-vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
 
 require("flutter-tools").setup({
     ui = { notification_style = "native" },
