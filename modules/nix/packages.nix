b3fb650dd2c9e5c6464e47c4953bda80b718cc9e
boatette <boatette@gmail.com>
2026-08-17 03:29:37 +1000

    fix: inherit files

--- modules/nix/packages.nix
+++ modules/nix/packages.nix
@@ -1,7 +1,7 @@
 { inputs, lib, ... }:
 
 let
-  files = ((inputs.import-tree.addPath ./..).filter (lib.hasSuffix ".pkg.nix")).files;
+  inherit (((inputs.import-tree.addPath ./..).filter (lib.hasSuffix ".pkg.nix"))) files;
 
   named = lib.listToAttrs (
     map (file: lib.nameValuePair (lib.removeSuffix ".pkg.nix" (baseNameOf file)) file) files
