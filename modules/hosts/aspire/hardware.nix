7a79b5c0cb317253476dcb93c6b91be4d0f60973
boatette <boatette@gmail.com>
2026-08-19 20:33:54 +1000

    feat: add swap hibernation

--- modules/hosts/aspire/hardware.nix
+++ modules/hosts/aspire/hardware.nix
@@ -23,6 +23,10 @@
         };
         kernelModules = [ "kvm-intel" ];
         extraModulePackages = [ ];
+
+        # recompute with `btrfs inspect-internal map-swapfile -r /.swapvol/swapfile` if swapfile is recreated
+        resumeDevice = "/dev/disk/by-id/nvme-WD_PC_SN740_SDDQNQD-512G-1014_2309F9403437-part2";
+        kernelParams = [ "resume_offset=32253184" ];
       };
 
       hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
