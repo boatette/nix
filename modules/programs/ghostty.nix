490f0ce6b42eaff58cc2af109c63b0be6fcd03f9
boatette <boatette@gmail.com>
2026-08-24 21:41:11 +1000

    refactor: remove systemd config

--- modules/programs/ghostty.nix
+++ modules/programs/ghostty.nix
@@ -1,32 +1,25 @@
 {
 
-  flake.modules.homeManager.ghostty =
-    { config, ... }:
-    {
-      programs.ghostty = {
-        enable = true;
+  flake.modules.homeManager.ghostty.programs.ghostty = {
+    enable = true;
 
-        settings = {
-          theme = "noctalia";
+    settings = {
+      theme = "noctalia";
 
-          font-family = "JetBrainsMono Nerd Font";
-          font-size = 12;
+      font-family = "JetBrainsMono Nerd Font";
+      font-size = 12;
 
-          window-padding-x = 14;
-          window-padding-y = 14;
-          window-decoration = "none";
+      window-padding-x = 14;
+      window-padding-y = 14;
+      window-decoration = "none";
 
-          background-opacity = 0.8;
+      background-opacity = 0.8;
 
-          confirm-close-surface = false;
-          gtk-single-instance = true;
-          quit-after-last-window-closed = false;
+      confirm-close-surface = false;
+      gtk-single-instance = true;
+      quit-after-last-window-closed = false;
 
-          window-inherit-working-directory = false;
-        };
-      };
-
-      xdg.configFile."systemd/user/graphical-session.target.wants/app-com.mitchellh.ghostty.service".source =
-        "${config.programs.ghostty.package}/share/systemd/user/app-com.mitchellh.ghostty.service";
+      window-inherit-working-directory = false;
     };
+  };
 }
