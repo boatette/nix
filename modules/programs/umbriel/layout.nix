f5749e0b1d2f17cbdb3db0515308bf880b2a6663
boatette <boatette@gmail.com>
2026-08-24 14:14:42 +1000

    feat: use vertical scrolling for portrait monitors

--- modules/programs/umbriel/layout.nix
+++ modules/programs/umbriel/layout.nix
@@ -1,3 +1,4 @@
+{ inputs, lib, ... }:
 {
   flake.modules.homeManager.umbriel.programs.umbriel.settings = {
     layout = {
@@ -21,6 +22,27 @@
         index = 10;
         layout.mode = "dwindle";
       }
-    ];
+    ]
+    ++
+      lib.concatMap
+        (
+          output:
+          map (index: {
+            inherit output index;
+            layout.scrolling.direction = "vertical";
+          }) (lib.range 1 10)
+        )
+        lib.attrNames
+        (
+          lib.filterAttrs (
+            m:
+            lib.elem (toString (m.transform or "normal")) [
+              "90"
+              "270"
+              "flipped-90"
+              "flipped-270"
+            ]
+          ) inputs.self.monitors
+        );
   };
 }
