3de8248cd253682c60310272d984fa0ceb88b9dd
boatette <boatette@gmail.com>
2026-08-24 20:46:36 +1000

    refactor: simplify shell config

--- modules/programs/shell/shell.nix
+++ modules/programs/shell/shell.nix
@@ -1,36 +1,16 @@
 {
-  flake.modules.nixos.shell =
-    { lib, ... }:
-    {
-      programs.fish.enable = true;
-      environment.shellAliases = lib.mkForce { };
+  flake.modules.nixos.shell.programs.fish.enable = true;
+
+  flake.modules.homeManager.shell.programs = {
+    fish = {
+      enable = true;
+      interactiveShellInit = builtins.readFile ./init.fish;
     };
 
-  flake.modules.homeManager.shell =
-    {
-      pkgs,
-      lib,
-      ...
-    }:
-    {
-      programs = {
-        fish = {
-          enable = true;
-
-          interactiveShellInit = lib.mkMerge [
-            (builtins.readFile ./init.fish)
-
-            (lib.mkAfter ''
-              ${lib.getExe pkgs.zoxide} init fish --cmd cd | source
-            '')
-          ];
-        };
-
-        zoxide = {
-          enable = true;
-          options = [ "--cmd cd" ];
-          enableFishIntegration = false;
-        };
-      };
+    zoxide = {
+      enable = true;
+      options = [ "--cmd cd" ];
+      enableFishIntegration = true;
     };
+  };
 }
