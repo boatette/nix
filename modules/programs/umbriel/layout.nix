3e189401f1e3d86622c1d18ac881982a04c6550d
boatette <boatette@gmail.com>
2026-08-26 15:20:15 +1000

    fix: set default column width to 0.5

--- modules/programs/umbriel/layout.nix
+++ modules/programs/umbriel/layout.nix
@@ -1,31 +1,37 @@
 { inputs, lib, ... }:
 {
-  flake.modules.homeManager.umbriel.programs.umbriel.settings.workspace = [
-    {
-      index = 10;
-      layout.mode = "dwindle";
-    }
-  ]
-  ++
-    lib.concatMap
-      (
-        output:
-        map (index: {
-          inherit output index;
-          layout.scrolling.direction = "vertical";
-        }) (lib.range 1 10)
-      )
-      (
-        lib.attrNames (
-          lib.filterAttrs (
-            _: m:
-            lib.elem (toString (m.transform or "normal")) [
-              "90"
-              "270"
-              "flipped-90"
-              "flipped-270"
-            ]
-          ) inputs.self.monitors
+  flake.modules.homeManager.umbriel.programs.umbriel.settings = {
+    layout = {
+      scrolling.default_width_fraction = 0.5;
+    };
+
+    workspace = [
+      {
+        index = 10;
+        layout.mode = "dwindle";
+      }
+    ]
+    ++
+      lib.concatMap
+        (
+          output:
+          map (index: {
+            inherit output index;
+            layout.scrolling.direction = "vertical";
+          }) (lib.range 1 10)
         )
-      );
+        (
+          lib.attrNames (
+            lib.filterAttrs (
+              _: m:
+              lib.elem (toString (m.transform or "normal")) [
+                "90"
+                "270"
+                "flipped-90"
+                "flipped-270"
+              ]
+            ) inputs.self.monitors
+          )
+        );
+  };
 }
