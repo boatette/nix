5c26b372a68e69e03828c071e8a7523fd30f8ba9
boatette <boatette@gmail.com>
2026-08-26 16:41:22 +1000

    refactor: remove blur and shadow configs

--- modules/programs/umbriel/appearance.nix
+++ modules/programs/umbriel/appearance.nix
@@ -3,18 +3,5 @@
     border_width = 1;
     outer_border_width = 1;
     corner_radius = 4;
-
-    blur = {
-      passes = 4;
-      radius = 6;
-      noise = 0.03;
-      saturation = 1.0;
-    };
-
-    shadow = {
-      enabled = false;
-      offset_x = 0;
-      offset_y = 0;
-    };
   };
 }
