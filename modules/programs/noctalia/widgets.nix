aaa9b2a1b425832ebb7822bc6a46099090af2aa7
boatette <boatette@gmail.com>
2026-08-26 16:42:25 +1000

    refactor: remove desktop widgets

--- modules/programs/noctalia/widgets.nix
+++ modules/programs/noctalia/widgets.nix
@@ -1,4 +1,3 @@
-{ inputs, ... }:
 {
   flake.modules.homeManager.noctalia.programs.noctalia.settings.widget = {
     battery = {
@@ -39,32 +38,4 @@
 
     workspaces.style = "minimal";
   };
-
-  flake.modules.homeManager.noctalia.programs.noctalia.settings.desktop_widgets =
-    let
-      monitors = inputs.self.monitors;
-      names = builtins.attrNames monitors;
-      primary = builtins.head (builtins.filter (n: monitors.${n}.primary or false) names ++ names);
-    in
-    {
-      widget_order = [ "desktop-widget-0000000000000001" ];
-
-      widget.desktop-widget-0000000000000001 = {
-        type = "boatette/binary-clock:desktop";
-        output = primary;
-
-        box_height = 240.0;
-        box_width = 432.0;
-        cx = 226.0;
-        cy = 950.0;
-
-        settings = {
-          background_radius = 4;
-          show_digits = false;
-          show_headers = false;
-          show_labels = false;
-          show_separators = false;
-        };
-      };
-    };
 }
