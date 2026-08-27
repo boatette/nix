935c72ca6829e0611b02e55443d271bb5f23d8b7
boatette <boatette@gmail.com>
2026-08-27 13:15:33 +1000

    feat(nvim): underline treesitter context

--- modules/programs/nvim/treesitter.nix
+++ modules/programs/nvim/treesitter.nix
@@ -13,5 +13,27 @@
       enable = true;
       settings.max_lines = 3;
     };
+
+    extraConfigLuaPost = ''
+      do
+          local group = vim.api.nvim_create_augroup("TreesitterContextUnderline", { clear = true })
+
+          local function underline()
+              local sp = vim.api.nvim_get_hl(0, { name = "Comment", link = false }).fg
+              for _, name in ipairs({ "TreesitterContextBottom", "TreesitterContextLineNumberBottom" }) do
+                  vim.api.nvim_set_hl(0, name, { underline = true, sp = sp })
+              end
+          end
+
+          vim.api.nvim_create_autocmd("User", {
+              pattern = "ColourschemeApplied",
+              group = group,
+              desc = "Underline the context after a theme change",
+              callback = underline,
+          })
+
+          underline()
+      end
+    '';
   };
 }
