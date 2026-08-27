8143f8be1d1cce3458896b89a4dfe8ad4bd3be16
boatette <boatette@gmail.com>
2026-08-27 21:32:27 +1000

    refactor: pack into dotted path

--- modules/system/gaming.nix
+++ modules/system/gaming.nix
@@ -1,13 +1,11 @@
 {
-  flake.modules.nixos.gaming = {
-    programs = {
-      steam = {
-        enable = true;
-        remotePlay.openFirewall = true;
-        dedicatedServer.openFirewall = true;
-      };
-
-      gamemode.enable = true;
+  flake.modules.nixos.gaming.programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
     };
+
+    gamemode.enable = true;
   };
 }
