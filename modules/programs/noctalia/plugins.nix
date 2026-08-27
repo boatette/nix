2d56a9cb37983e5230158e3a64ce95cfdfb68bfb
boatette <boatette@gmail.com>
2026-08-27 18:39:55 +1000

    refactor(noctalia): remove umbriel-workspace-watch plugin

--- modules/programs/noctalia/plugins.nix
+++ modules/programs/noctalia/plugins.nix
@@ -1,4 +1,3 @@
-{ inputs, ... }:
 {
   flake-file.inputs.noctalia-plugins = {
     url = "github:boatette/noctalia-plugins";
@@ -7,7 +6,6 @@
 
   flake.modules.homeManager.noctalia =
     {
-      pkgs,
       config,
       lib,
       ...
@@ -16,75 +14,67 @@
       inherit (config.constants) flakeDir;
     in
     {
-      home.packages = [
-        inputs.noctalia-plugins.packages.${pkgs.stdenv.hostPlatform.system}.umbriel-workspace-watch
-      ];
+      programs.noctalia.settings = {
+        plugins = {
+          enabled = [
+            "dotnetrob/cat"
+            "avivbintangaringga/nix-monitor"
+            "noctalia/wallhaven"
 
-      programs = {
-        noctalia = {
-          settings = {
-            plugins = {
-              enabled = [
-                "dotnetrob/cat"
-                "avivbintangaringga/nix-monitor"
-                "noctalia/wallhaven"
+            "boatette/auto-theme"
+          ];
 
-                "boatette/auto-theme"
-              ];
+          source = [
+            {
+              name = "official";
+              kind = "git";
+              location = "https://github.com/noctalia-dev/official-plugins";
+              enabled = true;
+            }
+            {
+              name = "community";
+              kind = "git";
+              location = "https://github.com/noctalia-dev/community-plugins";
+              enabled = true;
+            }
+            {
+              name = "Personal";
+              kind = "path";
+              location = "~/Projects/noctalia-plugins";
+              enabled = true;
+            }
+          ];
+        };
 
-              source = [
-                {
-                  name = "official";
-                  kind = "git";
-                  location = "https://github.com/noctalia-dev/official-plugins";
-                  enabled = true;
-                }
-                {
-                  name = "community";
-                  kind = "git";
-                  location = "https://github.com/noctalia-dev/community-plugins";
-                  enabled = true;
-                }
-                {
-                  name = "Personal";
-                  kind = "path";
-                  location = "~/Projects/noctalia-plugins";
-                  enabled = true;
-                }
-              ];
-            };
-
-            plugin_settings = {
-              "avivbintangaringga/nix-monitor" = {
-                branch = "nixos-unstable";
-                panel_placement = "floating";
-                show_update_available_notification = false;
-                update_command = ''nix run ~/nix#write-flake && nix flake update --flake ${flakeDir} && { git -C ${flakeDir} commit -m "chore: update flake lock" flake.lock || true; } && run0 nixos-rebuild switch --flake ${flakeDir}'';
-              };
-
-              "boatette/auto-theme".default_dynamic_scheme = "vibrant";
-              "dotnetrob/cat".panel_placement = "floating";
-              "noctalia/wallhaven" = {
-                browser_placement = "floating";
-                browser_position = "top_center";
-                download_dir = "~/Pictures/Wallpapers/Dynamic";
-              };
-            };
-
-            hooks = {
-              colors_changed = lib.mkBefore [
-                "noctalia msg plugin boatette/auto-theme:auto-theme all colors-changed"
-              ];
-
-              theme_mode_changed = [
-                "noctalia msg plugin boatette/auto-theme:auto-theme all theme-mode-changed"
-              ];
-
-              wallpaper_changed = [
-                ''noctalia msg plugin boatette/auto-theme:auto-theme all wallpaper-changed "$NOCTALIA_WALLPAPER_PATH"''
-              ];
-            };
+        plugin_settings = {
+          "avivbintangaringga/nix-monitor" = {
+            branch = "nixos-unstable";
+            panel_placement = "floating";
+            show_update_available_notification = false;
+            update_command = ''nix run ~/nix#write-flake && nix flake update --flake ${flakeDir} && { git -C ${flakeDir} commit -m "chore: update flake lock" flake.lock || true; } && run0 nixos-rebuild switch --flake ${flakeDir}'';
           };
+
+          "boatette/auto-theme".default_dynamic_scheme = "vibrant";
+          "dotnetrob/cat".panel_placement = "floating";
+          "noctalia/wallhaven" = {
+            browser_placement = "floating";
+            browser_position = "top_center";
+            download_dir = "~/Pictures/Wallpapers/Dynamic";
+          };
+        };
+
+        hooks = {
+          colors_changed = lib.mkBefore [
+            "noctalia msg plugin boatette/auto-theme:auto-theme all colors-changed"
+          ];
+
+          theme_mode_changed = [
+            "noctalia msg plugin boatette/auto-theme:auto-theme all theme-mode-changed"
+          ];
+
+          wallpaper_changed = [
+            ''noctalia msg plugin boatette/auto-theme:auto-theme all wallpaper-changed "$NOCTALIA_WALLPAPER_PATH"''
+          ];
         };
       };
     };
