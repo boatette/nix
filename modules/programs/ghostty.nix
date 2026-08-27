7ad46fcb6678b320b5b26b49967f3b4a54b0f4e4
boatette <boatette@gmail.com>
2026-08-27 14:08:34 +1000

    perf(ghostty): enable systemd user service

--- modules/programs/ghostty.nix
+++ modules/programs/ghostty.nix
@@ -1,25 +1,31 @@
 {
+  flake.modules.homeManager.ghostty =
+    { config, ... }:
+    {
+      programs.ghostty = {
+        enable = true;
 
-  flake.modules.homeManager.ghostty.programs.ghostty = {
-    enable = true;
+        settings = {
+          theme = "noctalia";
 
-    settings = {
-      theme = "noctalia";
+          font-family = "JetBrainsMono Nerd Font";
+          font-size = 12;
 
-      font-family = "JetBrainsMono Nerd Font";
-      font-size = 12;
+          window-padding-x = 14;
+          window-padding-y = 14;
+          window-decoration = "none";
 
-      window-padding-x = 14;
-      window-padding-y = 14;
-      window-decoration = "none";
+          background-opacity = 0.5;
 
-      background-opacity = 0.5;
+          confirm-close-surface = false;
+          gtk-single-instance = true;
+          quit-after-last-window-closed = false;
 
-      confirm-close-surface = false;
-      gtk-single-instance = true;
-      quit-after-last-window-closed = false;
+          window-inherit-working-directory = false;
+        };
+      };
 
-      window-inherit-working-directory = false;
+      xdg.configFile."systemd/user/graphical-session.target.wants/app-com.mitchellh.ghostty.service".source =
+        "${config.programs.ghostty.package}/share/systemd/user/app-com.mitchellh.ghostty.service";
     };
-  };
 }
