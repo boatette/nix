c2e53dd3b6c1f3b3c02cba1623e9407e64ed7d2c
boatette <boatette@gmail.com>
2026-08-28 12:49:33 +1000

    feat(umbriel): allow media binds when locked

--- modules/programs/umbriel/binds-media.nix
+++ modules/programs/umbriel/binds-media.nix
@@ -1,17 +1,24 @@
 {
-  flake.modules.homeManager.umbriel.programs.umbriel.settings.keybinds = {
-    "XF86AudioRaiseVolume" = "spawn:noctalia msg volume-up";
-    "XF86AudioLowerVolume" = "spawn:noctalia msg volume-down";
-    "XF86AudioMute" = "spawn:noctalia msg volume-mute";
-    "XF86AudioMicMute" = "spawn:noctalia msg mic-mute";
+  flake.modules.homeManager.umbriel.programs.umbriel.settings.keybinds =
+    let
+      locked = action: {
+        inherit action;
+        allow_when_locked = true;
+      };
+    in
+    {
+      "XF86AudioRaiseVolume" = locked "spawn:noctalia msg volume-up";
+      "XF86AudioLowerVolume" = locked "spawn:noctalia msg volume-down";
+      "XF86AudioMute" = locked "spawn:noctalia msg volume-mute";
+      "XF86AudioMicMute" = locked "spawn:noctalia msg mic-mute";
 
-    "XF86MonBrightnessUp" = "spawn:noctalia msg brightness-up";
-    "XF86MonBrightnessDown" = "spawn:noctalia msg brightness-down";
+      "XF86MonBrightnessUp" = locked "spawn:noctalia msg brightness-up";
+      "XF86MonBrightnessDown" = locked "spawn:noctalia msg brightness-down";
 
-    "Alt+XF86MonBrightnessUp" = "spawn:noctalia msg brightness-up 1";
-    "Alt+XF86MonBrightnessDown" = "spawn:noctalia msg brightness-down 1";
+      "Alt+XF86MonBrightnessUp" = locked "spawn:noctalia msg brightness-up 1";
+      "Alt+XF86MonBrightnessDown" = locked "spawn:noctalia msg brightness-down 1";
 
-    "Print" = "spawn:noctalia msg screenshot-fullscreen";
-    "Mod+Shift+S" = "spawn:noctalia msg screenshot-region";
-  };
+      "Print" = "spawn:noctalia msg screenshot-fullscreen";
+      "Mod+Shift+S" = "spawn:noctalia msg screenshot-region";
+    };
 }
