fce861802242180a11c8f489650abb5e91242709
boatette <boatette@gmail.com>
2026-08-28 19:15:40 +1000

    feat(gaming): add millennium overlay

--- flake.nix
+++ flake.nix
@@ -27,6 +27,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     import-tree.url = "github:denful/import-tree";
+    millennium.url = "github:SteamClientHomebrew/Millennium?dir=packages/nix";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixvim = {
       url = "github:nix-community/nixvim";
--- modules/system/gaming.nix
+++ modules/system/gaming.nix
@@ -1,11 +1,22 @@
+{ inputs, ... }:
 {
-  flake.modules.nixos.gaming.programs = {
-    steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-    };
+  flake-file.inputs.millennium.url = "github:SteamClientHomebrew/Millennium?dir=packages/nix";
 
-    gamemode.enable = true;
-  };
+  flake.modules.nixos.gaming =
+    { pkgs, ... }:
+    let
+      inherit (pkgs.stdenv.hostPlatform) system;
+    in
+    {
+      programs = {
+        steam = {
+          enable = true;
+          package = inputs.millennium.packages.${system}.millennium-steam;
+          remotePlay.openFirewall = true;
+          dedicatedServer.openFirewall = true;
+        };
+
+        gamemode.enable = true;
+      };
+    };
 }
