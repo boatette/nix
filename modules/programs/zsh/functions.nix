773af84c78feb96ab749be052cc9bca37867d306
boatette <boatette@gmail.com>
2026-08-29 00:50:38 +1000

    refactor(zsh): remove unused function

--- modules/programs/zsh/functions.nix
+++ modules/programs/zsh/functions.nix
@@ -2,8 +2,6 @@
   flake.modules.homeManager.zsh.programs.zsh.initContent = ''
     mkcd() { mkdir -p -- "$1" && cd "$1"; }
 
-    backup() { cp -- "$1" "$1.bak"; }
-
     nsh() { nix shell "''${@/#/nixpkgs#}"; }
 
     nrun() { local pkg="$1"; shift; nix run "nixpkgs#$pkg" -- "$@"; }
