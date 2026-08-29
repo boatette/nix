cdba05a9284772d9cb834838af2052206dce2f34
boatette <boatette@gmail.com>
2026-08-29 23:08:49 +1000

    refactor(umbriel): name files after config keys

--- modules/programs/umbriel/layout.nix
+++ modules/programs/umbriel/layout.nix
@@ -1,29 +1,7 @@
-{ inputs, lib, ... }:
-let
-  isPortrait =
-    m:
-    lib.elem (toString (m.transform or "normal")) [
-      "90"
-      "270"
-      "flipped-90"
-      "flipped-270"
-    ];
-
-  portraitOutputs = lib.attrNames (lib.filterAttrs (_: isPortrait) inputs.self.monitors);
-
-  verticalWorkspace = output: index: {
-    inherit output index;
-    layout.scrolling.direction = "vertical";
-  };
-in
 {
-  flake.modules.homeManager.umbriel.programs.umbriel.settings = {
-    layout = {
-      gap = 10;
+  flake.modules.homeManager.umbriel.programs.umbriel.settings.layout = {
+    gap = 10;
 
-      scrolling.default_width_fraction = 0.5;
-    };
-
-    workspace = lib.concatMap (output: map (verticalWorkspace output) (lib.range 1 10)) portraitOutputs;
+    scrolling.default_width_fraction = 0.5;
   };
 }
