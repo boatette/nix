2bcb764acd20c151ffead0f6add465a992da70b4
boatette <boatette@gmail.com>
2026-08-30 14:12:16 +1000

    refactor(zsh): make nfu a function instead of an alias

--- modules/programs/zsh/aliases.nix
+++ modules/programs/zsh/aliases.nix
@@ -45,7 +45,6 @@
           nrt = "nh os test";
           nrd = "nixos-rebuild dry-build --flake ${flakeDir}";
           ngl = "nixos-rebuild list-generations";
-          nfu = ''env --chdir ${flakeDir} nix run ${flakeDir}#write-flake && nix flake update --flake ${flakeDir} && git -C ${flakeDir} commit -m "chore: update flake lock" flake.lock'';
           ns = "nix search nixpkgs";
           nb = "nix path-info -rSh /run/current-system | sort -hk2 | tail -30";
           nrepl = "nix repl ${flakeDir}";
--- modules/programs/zsh/functions.nix
+++ modules/programs/zsh/functions.nix
@@ -1,11 +1,24 @@
 {
-  flake.modules.homeManager.zsh.programs.zsh.initContent = ''
-    mkcd() { mkdir -p -- "$1" && cd "$1"; }
+  flake.modules.homeManager.zsh =
+    { config, ... }:
+    let
+      inherit (config.constants) flakeDir;
+    in
+    {
+      programs.zsh.initContent = ''
+        mkcd() { mkdir -p -- "$1" && cd "$1"; }
 
-    nsh() { nix shell "''${@/#/nixpkgs#}"; }
+        nsh() { nix shell "''${@/#/nixpkgs#}"; }
 
-    nrun() { local pkg="$1"; shift; nix run "nixpkgs#$pkg" -- "$@"; }
+        nrun() { local pkg="$1"; shift; nix run "nixpkgs#$pkg" -- "$@"; }
 
-    unowned() { find "$@" -not -user "$USER"; }
-  '';
+        nfu() {
+          env --chdir ${flakeDir} nix run ${flakeDir}#write-flake \
+            && nix flake update --flake ${flakeDir} "$@" \
+            && git -C ${flakeDir} commit -m "chore: update flake lock''${1:+ ($*)}" flake.lock
+        }
+
+        unowned() { find "$@" -not -user "$USER"; }
+      '';
+    };
 }
