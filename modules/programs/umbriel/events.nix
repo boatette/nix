4311d1b79ecd19a60b4efb5ff1d99435b2255473
boatette <boatette@gmail.com>
2026-08-31 14:55:08 +1000

    feat(umbriel): add switch events

---
+++ modules/programs/umbriel/events.nix
@@ -0,0 +1,4 @@
+{
+  flake.modules.homeManager.umbriel.programs.umbriel.settings.events.lid_close =
+    "noctalia msg session lock-and-suspend";
+}
--- modules/programs/umbriel/general.nix
+++ modules/programs/umbriel/general.nix
@@ -1,5 +1,4 @@
 {
-  # TODO: add switch-events
   flake.modules.homeManager.umbriel.programs.umbriel.settings.general = {
     focus_on_activate = true;
     show_cheatsheet = false;
