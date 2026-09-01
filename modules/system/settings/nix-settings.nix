04349f60fda638fb9f37adc58304721ec8e3a955
boatette <boatette@gmail.com>
2026-09-01 22:33:30 +1000

    chore(nix): optimise the store and clean /tmp on boot

--- modules/system/settings/nix-settings.nix
+++ modules/system/settings/nix-settings.nix
@@ -2,20 +2,24 @@
   flake.modules.nixos.nix-settings =
     { lib, ... }:
     {
-      nix.settings = {
-        experimental-features = [
-          "nix-command"
-          "flakes"
-        ];
+      nix = {
+        settings = {
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
 
-        extra-substituters = [
-          "https://nix-community.cachix.org"
-          "https://noctalia.cachix.org"
-        ];
-        extra-trusted-public-keys = [
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-        ];
+          extra-substituters = [
+            "https://nix-community.cachix.org"
+            "https://noctalia.cachix.org"
+          ];
+          extra-trusted-public-keys = [
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+          ];
+        };
+
+        optimise.automatic = true;
       };
 
       systemd.services.nix-daemon.serviceConfig = {
