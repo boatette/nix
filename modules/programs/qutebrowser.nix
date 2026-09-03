0247118c3e01b5605f3df74199ab795d3bd0d162
boatette <boatette@gmail.com>
2026-09-03 15:01:39 +1000

    feat: add qutebrowser

---
+++ modules/programs/qutebrowser.nix
@@ -0,0 +1,24 @@
+{
+  flake.modules.homeManager.qutebrowser =
+    { config, ... }:
+    let
+      inherit (config.constants.fonts) mono;
+    in
+    {
+      programs.qutebrowser = {
+        enable = true;
+
+        settings = {
+          fonts.default_family = mono.name;
+          fonts.default_size = "${toString mono.size}pt";
+          colors.webpage.darkmode.enabled = true;
+          tabs.show = "multiple";
+        };
+
+        searchEngines = {
+          DEFAULT = "https://www.startpage.com/sp/search?query={}&prfe=f553b88948f2386e97944a17f918362dbaedfb6381ed1bcb684b1f32e856a0fbc55532682719dcb7ce08628290db7ab6f5fc795492b620e0fc5634f33adfb8e816eaf81977d31c2b9322224f1f2432f1";
+          nix = "https://search.nixos.org/packages?query={}";
+        };
+      };
+    };
+}
