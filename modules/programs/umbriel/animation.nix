14efddd2e24653427248dd45a8d637a657baab54
boatette <boatette@gmail.com>
2026-09-04 07:56:37 +1000

    feat(umbriel): steal lysec's animations

--- modules/programs/umbriel/animation.nix
+++ modules/programs/umbriel/animation.nix
@@ -1,82 +1,85 @@
 {
   flake.modules.homeManager.umbriel.programs.umbriel.settings.animation = {
     enabled = true;
-    duration_ms = 140;
-    curve = "appleEase";
+    duration_ms = 195;
+    curve = "cinematic";
 
-    beziers.appleEase = [
-      0.25
-      1.0
-      0.5
-      1.0
-    ];
+    beziers = {
+      cinematic = [
+        0.16
+        0.84
+        0.24
+        1.0
+      ];
+      window_flow = [
+        0.25
+        0.46
+        0.35
+        1.0
+      ];
+      workspace_flow = [
+        0.38
+        0.0
+        0.22
+        1.0
+      ];
+    };
 
-    springs.appleSpring = {
-      damping = 0.9;
-      stiffness = 420;
+    springs.apparition = {
+      damping = 0.70;
+      stiffness = 240;
     };
 
     windows_in = {
       enabled = true;
-      duration_ms = 140;
-      curve = "appleEase";
+      duration_ms = 205;
+      curve = "apparition";
       style = "popin";
-      scale = 0.9;
+      scale = 0.72;
     };
 
     windows_out = {
       enabled = true;
-      duration_ms = 110;
-      curve = "appleEase";
+      duration_ms = 165;
+      curve = "easeoutcubic";
       style = "fade";
     };
 
     windows_move = {
       enabled = true;
-      duration_ms = 170;
-      curve = "appleSpring";
+      duration_ms = 195;
+      curve = "window_flow";
     };
 
     workspaces = {
       enabled = true;
-      duration_ms = 190;
-      curve = "appleSpring";
+      duration_ms = 225;
+      curve = "workspace_flow";
     };
 
     overview = {
       enabled = true;
-      duration_ms = 190;
-      curve = "appleEase";
+      duration_ms = 280;
+      curve = "cinematic";
     };
 
     scratchpad = {
       enabled = true;
-      duration_ms = 170;
-      curve = "appleSpring";
-      dim = 0.3;
-      blur = true;
-      scale = 0.95;
+      duration_ms = 215;
+      curve = "easeinoutcubic";
+      dim = 0.42;
+      blur = false;
+      scale = 0.0;
       maximize = false;
       fullscreen = false;
     };
 
-    border = {
-      enabled = true;
-      duration_ms = 100;
-      curve = "linear";
-    };
-
-    dim_unfocused = {
-      enabled = false;
-      duration_ms = 140;
-      curve = "appleEase";
-      dim = 0.0;
-    };
+    border.enabled = false;
 
     layers = {
       enabled = true;
-      duration_ms = 110;
-      curve = "appleEase";
+      duration_ms = 175;
+      curve = "easeoutcubic";
     };
   };
 }
