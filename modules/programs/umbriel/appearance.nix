add2f500d9b34f96d5d3c6eac14ca0bec5075391
boatette <boatette@gmail.com>
2026-09-04 07:47:47 +1000

    refactor(umbriel): add blur settings

--- modules/programs/umbriel/appearance.nix
+++ modules/programs/umbriel/appearance.nix
@@ -3,5 +3,16 @@
     border_width = 1;
     outer_border_width = 1;
     corner_radius = 4;
+
+    blur = {
+      enabled = true;
+      optimized = true;
+      passes = 3;
+      radius = 3;
+      noise = 0.02;
+      brightness = 0.9;
+      contrast = 0.9;
+      saturation = 1.1;
+    };
   };
 }
