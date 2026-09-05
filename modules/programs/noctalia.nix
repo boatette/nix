7a893d2906ce1caa0468af9ef6392878ccc3a20f
boatette <boatette@gmail.com>
2026-09-05 14:07:27 +1000

    refactor(noctalia): use cachix flake

--- modules/programs/noctalia.nix
+++ modules/programs/noctalia.nix
@@ -1,7 +1,12 @@
+{ inputs, ... }:
 {
+  flake-file.inputs.noctalia.url = "github:noctalia-dev/noctalia/cachix";
+
   flake.modules.homeManager.noctalia =
     { pkgs, ... }:
     {
+      imports = [ inputs.noctalia.homeModules.default ];
+
       programs.noctalia = {
         enable = true;
         systemd.enable = true;
