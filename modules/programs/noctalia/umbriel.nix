feb8fed3e4eda2d2ae05a11a158eb70bcb8a4efc
boatette <boatette@gmail.com>
2026-09-05 14:36:19 +1000

    fix(umbriel): source the noctalia config

--- modules/programs/noctalia/umbriel.nix
+++ modules/programs/noctalia/umbriel.nix
@@ -1,40 +1,44 @@
 {
-  flake.modules.homeManager.umbriel.programs.umbriel.settings = {
-    include.files = [ "noctalia.toml" ];
+  flake.modules.homeManager.umbriel.programs.umbriel = {
+    validateConfig = false;
 
-    window_rule = [
-      {
-        match.app_id = "^dev\\.noctalia\\.Noctalia$";
-        default_floating = true;
-        default_size = [
-          1080
-          920
-        ];
-      }
+    settings = {
+      include.files = [ "noctalia.toml" ];
 
-      {
-        match.app_id = "^dev\\.noctalia\\.UmbrielSharePicker$";
-        default_floating = true;
-        default_size = [
-          800
-          600
-        ];
-      }
-    ];
+      window_rule = [
+        {
+          match.app_id = "^dev\\.noctalia\\.Noctalia$";
+          default_floating = true;
+          default_size = [
+            1080
+            920
+          ];
+        }
 
-    layer_rule = [
-      {
-        match.namespace = "^noctalia-(bar-[^\"]+|notification|dock|panel|attached-panel|osd|desktop-widget-[^\"]*)$";
-        blur = true;
-        blur_ignore_alpha = 0.5;
-        blur_popups = true;
-      }
+        {
+          match.app_id = "^dev\\.noctalia\\.UmbrielSharePicker$";
+          default_floating = true;
+          default_size = [
+            800
+            600
+          ];
+        }
+      ];
 
-      {
-        match.namespace = "^noctalia-window-switcher$";
-        blur = true;
-        blur_ignore_alpha = 0.5;
-      }
-    ];
+      layer_rule = [
+        {
+          match.namespace = "^noctalia-(bar-[^\"]+|notification|dock|panel|attached-panel|osd|desktop-widget-[^\"]*)$";
+          blur = true;
+          blur_ignore_alpha = 0.5;
+          blur_popups = true;
+        }
+
+        {
+          match.namespace = "^noctalia-window-switcher$";
+          blur = true;
+          blur_ignore_alpha = 0.5;
+        }
+      ];
+    };
   };
 }
