cf9a8b88fa7165faef0481135d130421de9a72ce
boatette <boatette@gmail.com>
2026-09-14 01:31:57 +1000

    fix(selector): disable blur

--- modules/programs/selector/umbriel.nix
+++ modules/programs/selector/umbriel.nix
@@ -2,9 +2,9 @@
   flake.modules.homeManager.umbriel.programs.umbriel.settings.layer_rule = [
     {
       match.namespace = "^selector$";
-      blur = true;
       blur_ignore_alpha = 0.05;
       blur_optimized = false;
+      blur = false;
     }
   ];
 }
