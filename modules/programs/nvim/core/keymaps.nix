a1060b0a5216a0c605e685e918f48468a468db61
boatette <boatette@gmail.com>
2026-09-14 16:16:52 +1000

    refactor(nvim): remove leader R map

--- modules/programs/nvim/core/keymaps.nix
+++ modules/programs/nvim/core/keymaps.nix
@@ -31,14 +31,6 @@
         (cmd "v" "p" "\"_dP`[v`]=" "Paste without yank")
         (cmd "n" "J" "mzJ`z" "Better join")
 
-        (lua "n" "<leader>R" /* lua */ ''
-          function()
-              local session = vim.fn.stdpath("state") .. "/restart_session.vim"
-              vim.cmd("mksession! " .. vim.fn.fnameescape(session))
-              vim.cmd("restart source " .. vim.fn.fnameescape(session))
-          end
-        '' "Restart Neovim")
-
         (
           lua "n" "i" /* lua */ ''
             function()
