a0db171b1c1ad5e733acc8b99157fb76d9be132e
boatette <boatette@gmail.com>
2026-09-19 23:17:31 +1000

    feat(zsh): add dotfiles alias to switch to new git worktree

--- modules/programs/zsh/functions.nix
+++ modules/programs/zsh/functions.nix
@@ -1,13 +1,33 @@
 {
-  flake.modules.homeManager.zsh = {
-    programs.zsh.initContent = ''
-      mkcd() { mkdir -p -- "$1" && cd "$1"; }
+  flake.modules.homeManager.zsh =
+    { config, ... }:
+    let
+      inherit (config.constants) flakeDir;
+    in
+    {
+      programs.zsh.initContent = ''
+        mkcd() { mkdir -p -- "$1" && cd "$1"; }
 
-      nsh() { nix shell "''${@/#/nixpkgs#}"; }
+        nsh() { nix shell "''${@/#/nixpkgs#}"; }
 
-      nrun() { local pkg="$1"; shift; nix run "nixpkgs#$pkg" -- "$@"; }
+        nrun() { local pkg="$1"; shift; nix run "nixpkgs#$pkg" -- "$@"; }
 
-      unowned() { find "$@" -not -user "$USER"; }
-    '';
-  };
+        unowned() { find "$@" -not -user "$USER"; }
+
+        dotfiles() {
+          local dir
+
+          git -C ${flakeDir} worktree prune
+          dir=$(git -C ${flakeDir} worktree list --porcelain |
+            awk '/^worktree /{w=$2} /^branch refs\/heads\/dotfiles$/{print w; exit}')
+
+          [[ -n $dir ]] || {
+            dir=${flakeDir}-dotfiles
+            git -C ${flakeDir} worktree add "$dir" dotfiles || return
+          }
+
+          cd "$dir"
+        }
+      '';
+    };
 }
