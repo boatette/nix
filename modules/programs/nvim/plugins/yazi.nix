356b2b51613ab2813fc32915cce0adc4a8257be9
boatette <boatette@gmail.com>
2026-09-19 20:04:27 +1000

    refactor(nvim): use yazi mappings instead of nvf keymaps

--- modules/programs/nvim/plugins/yazi.nix
+++ modules/programs/nvim/plugins/yazi.nix
@@ -1,18 +1,11 @@
-{ inputs, ... }:
 {
   flake.modules.nvf.nvim.vim = {
     utility.yazi-nvim = {
       enable = true;
       setupOpts.open_for_directories = true;
-    };
 
-    keymaps =
-      let
-        inherit (inputs.self.lib.nvim) cmd;
-      in
-      [
-        (cmd "n" "<leader>e" "<cmd>Yazi<cr>" "File explorer")
-        (cmd "n" "<leader>E" "<cmd>Yazi cwd<cr>" "File explorer (cwd)")
-      ];
+      mappings.openYazi = "<leader>e";
+      mappings.openYaziDir = "<leader>E";
+    };
   };
 }
